c1f29c8977b99e67ac90faa862a2a9fd169e4bc3
timlin940 <aa0909095679@gmail.com>
2026-03-30 18:40:13 +0800

    啥也沒做

--- Data_preprocess.py
+++ Data_preprocess.py
@@ -1,2 +1,9 @@
 import numpy as np
 import pandas as pd
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+project_dir = script_dir.parent
+
+train = pd.read_csv(project_dir / "Data" / "train.csv")
+test = pd.read_csv(project_dir / "Data" / "test.csv")
